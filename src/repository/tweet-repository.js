f23946c3c5f0bd944a84431f0da14270e1ae0d61
hasnatulalam <hasnatualam056@gmail.com>
2023-12-05 01:40:33 +0600

    common crudrepository

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,7 +1,11 @@
 import Tweet from '../models/tweet.js';
+import CrudRepository from './crud-repository.js';
 
 
-class TweetRepository {
+class TweetRepository  extends CrudRepository{
+   constructor(){
+      super(Tweet)
+   }
   
  
     async create (data){
@@ -12,14 +16,7 @@ class TweetRepository {
             console.log(error)
          }
     }
-    async get(id){
-       try {
-        const tweet=await Tweet.findById(id);
-       return tweet
-       } catch (error) {
-        console.log(error)
-       }
-    }
+    
     async getWithComments(id){
       try {
          const tweet=await Tweet.findById(id).populate({path:'comments'});
@@ -37,14 +34,7 @@ class TweetRepository {
             
         }
     }
-    async destroy(id){
-     try {
-        const tweet =await Tweet.findByIdAndRemove(id) 
-        return tweet
-     } catch (error) {
-        console.log(error)
-     }
-    }
+   
     async getAll(offset,limit){
          try {
             const tweet= await Tweet.find().skip(offset).limit(limit);
